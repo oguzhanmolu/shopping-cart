ed2582d767ff5b7fff8bc5319f86ea145917a7b6
oguzhanmolu <oguzhanmolu@pop-os.localdomain>
2023-04-10 12:17:10 +0300

    add webpack for env import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <h1>test</h1>;
+  const apiKey = process.env.REACT_APP_API_KEY;
+
+  // Fetch API
+  const fetchAPI = () => {};
+
+  return <h1>{apiKey}</h1>;
 }
 
 export default App;
